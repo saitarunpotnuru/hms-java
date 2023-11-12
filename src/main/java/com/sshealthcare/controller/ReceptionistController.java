3e67386e4890e972c29599bd8623ccf3a0d9939f
SwarnaYeluru <swarnayeluru1025@gmail.com>
2023-11-12 20:22:28 +0530

    receptionist get all

--- src/main/java/com/sshealthcare/controller/ReceptionistController.java
+++ src/main/java/com/sshealthcare/controller/ReceptionistController.java
@@ -1,10 +1,16 @@
 package com.sshealthcare.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sshealthcare.model.Receptionist;
@@ -46,7 +52,14 @@ public class ReceptionistController {
 			
 			return receptionistService.insert(receptionist);
 			
-		}
-
-
+	}
+	
+	@GetMapping("/all")
+	public List<Receptionist> getAllReceptionists(
+			@RequestParam(value="page",required = false,defaultValue = "0") Integer page,
+			@RequestParam(value="size",required = false,defaultValue = "1000000") Integer size) {
+		
+		Pageable pageable =  PageRequest.of(page, size);
+		return receptionistService.getAllreceptionists(pageable);
+	}
 }
--- src/main/java/com/sshealthcare/service/ReceptionistService.java
+++ src/main/java/com/sshealthcare/service/ReceptionistService.java
@@ -1,6 +1,9 @@
 package com.sshealthcare.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.sshealthcare.model.Receptionist;
@@ -15,6 +18,10 @@ public class ReceptionistService {
 	public Receptionist insert(Receptionist receptionist) {
 		return receptionistRepository.save(receptionist);
 		
+	}
+
+	public List<Receptionist> getAllreceptionists(Pageable pageable) {
+		return receptionistRepository.findAll(pageable).getContent();
 	}
 
 }

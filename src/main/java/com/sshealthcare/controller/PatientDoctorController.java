571f555940c5e85eafee3141325e7778ce871b57
SwarnaYeluru <swarnayeluru1025@gmail.com>
2023-11-13 20:46:25 +0530

    appointment get all

--- src/main/java/com/sshealthcare/controller/PatientDoctorController.java
+++ src/main/java/com/sshealthcare/controller/PatientDoctorController.java
@@ -1,17 +1,24 @@
 package com.sshealthcare.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sshealthcare.exception.InvalidIdException;
 import com.sshealthcare.model.Doctor;
 import com.sshealthcare.model.Patient;
 import com.sshealthcare.model.PatientDoctor;
+import com.sshealthcare.model.Receptionist;
 import com.sshealthcare.service.DoctorService;
 import com.sshealthcare.service.PatientDoctorService;
 import com.sshealthcare.service.PatientService;
@@ -45,5 +52,15 @@ public class PatientDoctorController {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
 	}
+	
+	//getting all appointments
+		@GetMapping("/all")
+		public List<PatientDoctor> getAllPatientDoctors(
+				@RequestParam(value="page",required = false,defaultValue = "0") Integer page,
+				@RequestParam(value="size",required = false,defaultValue = "1000000") Integer size) {
+			
+			Pageable pageable =  PageRequest.of(page, size);
+			return patientDoctorService.getAllpatientDoctors(pageable);
+		}
 
 }
--- src/main/java/com/sshealthcare/service/PatientDoctorService.java
+++ src/main/java/com/sshealthcare/service/PatientDoctorService.java
@@ -1,6 +1,9 @@
 package com.sshealthcare.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.sshealthcare.model.PatientDoctor;
@@ -14,6 +17,10 @@ public class PatientDoctorService {
 	//adding
 	public PatientDoctor assignPatientDoctor(PatientDoctor patientDoctor) {
 		return patientDoctorRepository.save(patientDoctor);
+	}
+
+	public List<PatientDoctor> getAllpatientDoctors(Pageable pageable) {
+		return patientDoctorRepository.findAll(pageable).getContent();
 	}
 
 }
